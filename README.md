0acbd586961ef9ea4954c1f65ba2c3f2463becd4
Akash Pillai <akashp807@gmail.com>
2021-05-02 22:21:33 -0500

    Add README and deploy script

---
+++ README.md
@@ -0,0 +1,19 @@
+# Personal Website
+This is the second iteration of my personal website, built with Vue + Vite and designed with Tailwind CSS.
+
+## Development
+```
+$ yarn dev
+```
+
+## Building for production
+```
+$ yarn build
+```
+
+## Deployment
+This project is set up to deploy to GitHub Pages, so use the included deploy script `deploy.sh` to build the project and push the output to the GitHub Pages repo:
+```
+$ sh deploy.sh
+```
+You can view the current build [here](https://github.com/FestiveAkp/festiveakp.github.io).
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on error
+set -e
+
+# build static assets
+rm -rf dist
+yarn build
+
+# write and stage files
+cd dist
+git init
+echo "akash.dev" > CNAME
+
+cat << EOL > README.md
+# festiveakp.github.io
+This repo holds the static build output for my personal website, which is deployed through GitHub Pages.
+You can find the actual source repo [here](https://github.com/FestiveAkp/website-v2).
+EOL
+
+git add -A
+git commit -m "deploy: $(date +"%F %r")"
+
+# push to repo festiveakp.github.io
+git push -f "https://github.com/FestiveAkp/festiveakp.github.io.git" master
+
+cd ..
+echo ""
+echo ">> Deploy successful"
