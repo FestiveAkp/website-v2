d6eeea73daf88dbb893b2bc1e0981182b0afdd5f
Akash Pillai <akashp807@gmail.com>
2026-03-24 17:58:01 -0500

    Add 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [icon()],
     fonts: [
         {
             name: 'Inter',
